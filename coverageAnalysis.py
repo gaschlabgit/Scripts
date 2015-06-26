5996b358172133e82cfdd2540fe60973960dfd0e
Mike Place <mplace@wisc.edu>
2015-06-26 10:48:54 -0500

    update

--- coverageAnalysis.py
+++ coverageAnalysis.py
@@ -1,16 +1,13 @@
 #!/home/mplace/anaconda3/bin/python
 """
-Created May 29th 2015
-
-@author: mplace
-
 Program: coverageAnalysis.py 
 
-Purpose: Use samtools to count reads in 1kb window, stepping 100bp.
+Purpose: Use samtools to count reads in a user specified window size.
+         Default window size is 500 bp.
 
 Input  : bam file
  
-Output: Table where columns are position, count, %GC
+Output : Table where columns are chrom, start_position, end_position, count, count/median_count, log2(count/median_count)
 
 dependencies: samtools, must be in path
 
@@ -29,41 +26,32 @@ data looks like:
 
 qplot(d$pos,d$ct )
 
+Created June 26th 2015
 
+@author: mplace
 
 """
 import argparse 
+import math
 import os
 import sys
 import re
 import subprocess 
-from collections import defaultdict     
+from collections import defaultdict  
+from statistics import median
 
-chromSet = {"ref|NC_001133|", "ref|NC_001134|", "ref|NC_001135|", "ref|NC_001136|", 
-             "ref|NC_001137|", "ref|NC_001138|", "ref|NC_001139|", "ref|NC_001140|",
-             "ref|NC_001141|", "ref|NC_001142|", "ref|NC_001143|", "ref|NC_001144|",
-             "ref|NC_001145|", "ref|NC_001146|", "ref|NC_001147|", "ref|NC_001148|", 
-             "ref|NC_001224|", "ref|NC_001224|"
-} 
+# key = chromosome  each value is a dict of lists, the keys are 'pos', 'cnt'  
+chrom    = defaultdict(dict)
 
-chromLen = { "ref|NC_001133|" : 230218, "ref|NC_001134|" : 813184, "ref|NC_001135|" : 316620,
-             "ref|NC_001136|" : 1531933, "ref|NC_001137|" : 576874, "ref|NC_001138|" : 270161,
-             "ref|NC_001139|" : 1090940, "ref|NC_001140|" : 562643, "ref|NC_001141|" : 439888,
-             "ref|NC_001142|" : 745751, "ref|NC_001143|" : 666816, "ref|NC_001144|" : 1078177,
-             "ref|NC_001145|" : 924431, "ref|NC_001146|" : 784333, "ref|NC_001147|" : 1091291,
-             "ref|NC_001148|" : 948066, "ref|NC_001224|" : 85779 
-}
-
-# key = chromosome  each value is a list of reads counts by window
-chromDict = defaultdict(list)
 
 def main():
     """
-    Read in bam file, parse and count reads for each 1kb non-overlaping bins.
+    Read in bam file, parse and count reads for each non-overlaping bin.
     """
     cmdparser = argparse.ArgumentParser(description="Calculate Copy Number Variants using a sorted bam file.",
                                         usage='%(prog)s <file.bam>' ,prog='coverageAnalysis.py'  )
-    cmdparser.add_argument('-b', '--bam',  action='store',      dest='BAM',  help='bam file')         
+    cmdparser.add_argument('-b', '--bam',  action='store',      dest='BAM',  help='bam file', metavar='') 
+    cmdparser.add_argument('-w', '--win',  action='store',      dest='WIN',  help='Window size for counting reads', metavar='')        
     cmdparser.add_argument('-i', '--info', action='store_true', dest='INFO', help='Program information')
     cmdResults = vars(cmdparser.parse_args())
     
@@ -75,17 +63,14 @@ def main():
     if cmdResults['INFO']:
         print("\n coverageAnalysis.py")
         print("\n Purpose: Copy Number Analysis.")
-        print("\t  parse and count reads for each 1kb non-overlaping bins.")
+        print("\t  parse and count reads for non-overlaping bins.")
         print(" Required Parameters: -b sampleName.bam")
+        print(" Optional Parameters: -w window size")
         print("\n Input : Bam file")
-        print("\n\tAssumes Chrom names are like: ref|NC_001133|\n")
-        print(" To run:  /home/GLBRCORG/mplace/scripts/coverageAnalysis.py -b samplename.bam\n")
-        print(" Output: Tab delimited table w/ columns WindowStartPosition WindowCount %GC ")
-        print("         Start of new chromosome marked by chromosome name. ")
+        print(" To run:  /home/GLBRCORG/mplace/scripts/coverageAnalysis.py -b samplename.bam \n")
+        print(" Output: Tab delimited table w/ columns Chrom Start End Count Count/median log2(Count/median)  ")
         print(" Example:")        
-        print("\tref|NC_001133|" )
-        print("\t1010    647     0.424419")
-        print("\t2022    1523    0.354627")
+        print("\tref|NC_001133|    1010    1510    647     0.424419  -1.2364")
         print("\n")  
         print(" To visual with R:")
         print(" Edit file: add header and split by chromosome")
@@ -96,10 +81,15 @@ def main():
 
         sys.exit(1)
     
+    if cmdResults['WIN']:
+        window = int(cmdResults['WIN'])
+    else:
+        window = 500          # default window size
+    
     if cmdResults['BAM']:
         infile = cmdResults['BAM']   
-        samName = re.sub(r"bam", "sam", infile )        
-        
+        samName = re.sub(r"bam", "sam", infile )   
+        # create sam file for parsing        
         if ( not os.path.exists(samName) ):
             cmd  = [ 'samtools', 'view', '-o', samName, infile ]
             output  = subprocess.Popen( cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE ).communicate()
@@ -107,44 +97,72 @@ def main():
         first     = 0
         chrName   = ""
         startPos  = 0
-        endPos    = 1000
+        endPos    = window
         ctr       = 0
-        gcContent = 0
-        totLen    = 0
         with open(samName, 'r') as reads:
             for i in reads:
                 line = i.split()
+                # find first chromosome
                 if first == 0:
-                    chrName   = line[2]
-                    first     = 1
+                    chrName    = line[2]
+                    first      = 1
                     ctr       += 1
-                    gcContent += len(re.findall("G|C",line[9]))
-                    totLen    += len(line[9])
-                    print("%s" %(chrName) )
+                    chrom[chrName]['cnt'] = []
+                    chrom[chrName]['pos'] = []
+                # when chromosome changes
                 elif chrName != line[2]:
                     chrName   = line[2]
                     startPos  = 0
-                    endPos    = 1000
+                    endPos    = window
                     ctr       = 1
-                    gcContent += len(re.findall("G|C",line[9]))
-                    totLen    += len(line[9])
-                    print("%s" %(chrName) )
+                    chrom[chrName]['cnt'] = []
+                    chrom[chrName]['pos'] = []    
+                # count if within current window
                 elif ( int(line[3]) < endPos and int(line[3]) >= startPos ):
                     ctr += 1
-                    gcContent += len(re.findall("G|C",line[9]))
-                    totLen    += len(line[9])
+                # change to next window
                 else:
                     ctr += 1
-                    gcContent += len(re.findall("G|C",line[9]))
-                    totLen    += len(line[9])    
-                    percentage = gcContent/totLen
-                    print( "%s\t%d\t%f" %(line[3], ctr, percentage))                                
-                    chromDict[chrName].append(ctr)
-                    startPos += 1000
-                    endPos   += 1000
+                    windowPos = int(line[3]) - window
+                    
+                    chrom[chrName]['cnt'].append(ctr)
+                    chrom[chrName]['pos'].append(windowPos)
+                                        
+                    startPos += window
+                    endPos   += window
                     ctr       = 1
-                    gcContent = 0
-                    totLen    = 0
+               
+        # sometimes mapped reads have a chromosome named "*", get rid of those
+        remove = [ key for key in chrom if key == '*']
+        for key in remove:
+            del chrom[key]
+        
+        chromList = chrom.keys()
+            
+        # print file header
+        print("%s\t%s\t%s\t%s\t%s\t%s" %("chr", "Startpos","EndPos", "cnt", "cnt/median", "log2(cnt/median)" ))
+                  
+        # loop through all chromosomes in dictionary
+        for item in chromList:
+            cntMedian = median ( chrom[item]['cnt'] )
+
+            for position, count in zip(chrom[item]['pos'], chrom[item]['cnt']):
+                end = int(position) + window
+                if count != 0:
+                    ratio = count/cntMedian
+                else:
+                    ratio = 0
+                
+                if ratio == 0:
+                    logRatio = "NA"
+                else:
+                    logRatio = math.log2(ratio)
+                # write results to stdout
+                print("%s\t%s\t%s\t%s\t%s\t%s" %(item, int(position), end, count, ratio, logRatio  ) )
+                
+            
+            
+        
           
 if __name__ == "__main__":
     main()
